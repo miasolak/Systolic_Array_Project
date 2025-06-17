91870c8dc760aa67a815c722ed1b65a7a08c90db
Mia Solak <miasolak@gmail.com>
2025-06-17 17:55:18 +0200

    FSM radi za N!=M

--- HW/fsm.v
+++ HW/fsm.v
@@ -1,15 +1,17 @@
-module fsm (
+module fsm #(parameter M = 3, N = 4)(
     input wire clk_i,
     input wire reset_i,
     output reg en_a_o,
     output reg en_b_o,
     output reg en_x_o
 );
-
+    reg [3:0] max;
+    reg [3:0] min;
+    
     localparam Idle    = 2'b00;
     localparam Load_AB = 2'b01;
+    localparam Wait    = 2'b11;
     localparam Load_X  = 2'b10;
-  //  localparam Wait    = 2'b11;
 
     reg [1:0] state, next_state;
     reg [1:0] counter;
@@ -21,7 +23,6 @@ module fsm (
             state <= Idle;
             counter <= 0;
         end else begin
-        
         if (state != next_state)      // ovo smisli jel moze drugacije nekako PITAAAJ
               counter <= 0;
         else
@@ -34,16 +35,36 @@ module fsm (
 
     // Kombinaciona logika
     always @(*) begin
+    
+     if (M > N) begin
+            max = M;
+            min = N;
+        end else begin
+            max = N;
+            min = M;
+        end
+        
         case (state)
             Idle: next_state = Load_AB;
             Load_AB: begin
-                if (counter == 2) // PROMENA: sad čekaš 0,1,2,3 (3 takta)
-                    next_state = Load_X;
-                else
-                    next_state = Load_AB;
+                    if (counter == min - 1) begin
+                        if (M == N)
+                            next_state = Load_X;  // odmah ide u Load_X
+                        else
+                            next_state = Wait;    // ide u Wait ako treba
+                    end else begin
+                        next_state = Load_AB;     // još nije gotovo
+                    end
+            
+            end
+            Wait: begin
+                    if (counter == max - min - 1) // PROMENA: sad čekaš 0,1,2,3 (3 takta)
+                        next_state = Load_X;
+                    else
+                        next_state = Wait;
             end
             Load_X: begin
-                if (counter == 2)
+                if (counter == N - 1)
                     next_state = Load_AB;
                 else
                     next_state = Load_X;
@@ -63,6 +84,16 @@ module fsm (
             Load_AB: begin
                 en_a_o = 1;
                 en_b_o = 1;             //OBAVEZNO OVDE VIDI ZNAKOVE - DA LI JE = ILI <= ???? 
+            end
+            Wait: begin
+                if (M > N) begin
+                en_a_o = 1;
+                en_b_o = 0;
+            end else begin
+                en_a_o = 0;
+                en_b_o = 1;
+            end 
+                
             end
             Load_X: begin
                 en_x_o = 1;             //odgovor: ovde je always bez posedge dakle kombinaciona dakle ide ovo =
